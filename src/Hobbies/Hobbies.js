c66dc83ab1f3be55233f22bff40fbf9196507ea6
Michael Martinelli <turtelboy123@gmail.com>
2024-09-05 06:12:57 -0600

    add screenshots of projects, as well as include links for my projects.

--- src/Hobbies/Hobbies.js
+++ src/Hobbies/Hobbies.js
@@ -7,10 +7,17 @@ const Hobbies = () => {
                 <li>Gym</li>
                 <p>Going to the gym is a huge part of my life.
                     It helps me keep my mind away from day to day activites, and is a place where I can constantly
-                    better myself, and take a break from day to day activites.
+                    better myself, and take a break from day to day activites. I lift weights, and have
+                    been practicing Muay Thai, a combat sport originated in Thailand.
                 </p>
-                <p>
-
+                <li>Games</li>
+                <p>I'm a huge gamer! League, World of Warcraft, Elden Ring, these are some of my favorite games to play.
+                    I also frequent playing Magic the Gathering.
+                </p>
+                <li>Entertainment</li>
+                <p>There's a lot of tv shows I like. I range from anime to tv drama like Breaking Bad.
+                    My personal favorite piece of entertainment is Berserk, a japenese graphic novel and
+                    anime.
                 </p>
 
             </div>
